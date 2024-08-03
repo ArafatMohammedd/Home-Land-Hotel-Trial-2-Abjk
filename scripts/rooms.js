e2ac251e251c055ba6358af30d6ebed02825a94f
ArafatMohammedd <mab818211@gmail.com>
2024-08-03 16:56:00 +0300

    Update rooms.js

--- scripts/rooms.js
+++ scripts/rooms.js
@@ -4,26 +4,26 @@ export const rooms = [{
     priceFor: 1,
     bedType: 'Queen',
     roomSize: 20,
-    phoneNumber: 251966701153
+    phoneNumber: 0222114292
 },{
     img: "images/Room-3.jpg",
     roomDetail: "Standard Room",
     priceFor: 1,
     bedType: 'Double Large',
     roomSize: 24,
-    phoneNumber: 251966701153
+    phoneNumber: 0222111131
 },{
     img: "images/Room-4.jpg",
     roomDetail: "Deluxe Room",
     priceFor: 2,
     bedType: 'Queen',
     roomSize: 25,
-    phoneNumber: 251966701153
+    phoneNumber: 0222114292
 },{
     img: "images/Room-1.jpg",
     roomDetail: "Twin Room",
     priceFor: 2,
     bedType: 'King',
     roomSize: 30,
-    phoneNumber: 251966701153
+    phoneNumber: 0222114292
 }];
