47b0ab1cc55cd4cf2cecd5678b74429cafbaf45c
ArafatMohammedd <mab818211@gmail.com>
2024-08-05 18:44:48 +0300

    Update roomsPage.js

--- scripts/roomsPage.js
+++ scripts/roomsPage.js
@@ -1,53 +1,20 @@
+import { rooms } from "./rooms.js";
+
 document.addEventListener('DOMContentLoaded', function() {
     const roomsContainer = document.getElementById('rooms-container');
 
-    const roomData = [
-        {
-            title: "Single Room",
-            priceFor: "Price For: 1 Adult",
-            bedType: " Bed Type: Queen ",
-            roomSize: "Room Size: 20m2",
-            phoneNumber: "+251966701153",
-            img: 'images/Room-1.jpg'
-        },
-        {
-            title: "Double Room",
-            priceFor: "Price For: 1 Adult",
-            bedType: " Bed Type: Queen ",
-            roomSize: "Room Size: 20m2",
-            phoneNumber: "+251966701154",
-            img: 'images/Room-3.jpg'
-        },
-        {
-            title: "Suite",
-            priceFor: "Price For: 1 Adult",
-            bedType: " Bed Type: Queen ",
-            roomSize: "Room Size: 20m2",
-            phoneNumber: "+251966701155",
-            img: 'images/Room-4.jpg'
-        },
-        {
-            title: "Family Room",
-            priceFor: "Price For: 1 Adult",
-            bedType: " Bed Type: Queen ",
-            roomSize: "Room Size: 20m2",
-            phoneNumber: "+251966701156",
-            img: 'images/Room-1.jpg'
-        }
-    ];
-
-    roomData.forEach(room => {
+    rooms.forEach(room => {
         const roomElement = document.createElement('div');
         roomElement.classList.add('room');
 
-        roomElement.innerHTML = `
-            <img src="${room.img}" alt="${room.title}">
-            <h1>${room.title}</h1>
-            <p class="description">${room.priceFor}</p>
-            <p class="description">${room.bedType}</p>
-            <p class="description">${room.roomSize}</p>                        
+        roomElement.innerHTML += `
+            <img src="${room.img}" alt="${room.roomDetail}">
+            <h1>${room.roomDetail}</h1>
+            <p class="description">Price For: ${room.priceFor}</p>
+            <p class="description"> Bed Type: ${room.bedType}</p>
+            <p class="description"> Room Size: ${room.roomSize}</p>                        
             <p>Phone number: ${room.phoneNumber}</p>
-            <button><a href="tel:+251966701156">Call To Book</a></button>
+            <button><a href="tel:0222111131">Call To Book</a></button>
         `;
 
         roomsContainer.appendChild(roomElement);
